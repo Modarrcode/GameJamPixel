aea9ae1b4ed60a04d0d8bc93e90a8bb0c9170cab
SQLRejection <32144044+SQLRejection@users.noreply.github.com>
2024-05-11 15:44:06 -0400

    basic collectable and ui

---
+++ CollectableUI.gd
@@ -0,0 +1,14 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	text = "0"
+	pass # Replace with function body.
+
+
+
+
+func _on_area_2d_collected():
+	text = str(int(text) + 1)
+	pass # Replace with function body.
